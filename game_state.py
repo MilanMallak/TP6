aee70cbb56e0d4db088f1a9027b6ab424226999b
Milan Mallak <mallakm@educ.college-montreal.qc.ca>
2026-06-18 17:25:23 -0400

    Update game_state.py

--- game_state.py
+++ game_state.py
@@ -3,7 +3,6 @@ from enum import Enum
 class GameState(Enum):
    NOT_STARTED = 0
    NOT_OPENED = 1
-   INTRUCTIONS = 2
-   ROUND_ACTIVE = 3
-   ROUND_DONE = 4
-   GAME_OVER = 5
+   ROUND_ACTIVE = 2
+   ROUND_DONE = 3
+   GAME_OVER = 4
